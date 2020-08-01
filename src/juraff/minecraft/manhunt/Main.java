a6c49abc9bf6430873010c63c1c098a78e208b00
BuildTools <unconfigured@null.spigotmc.org>
2020-07-31 22:38:50 -0700

    hello world

--- src/juraff/minecraft/manhunt/Main.java
+++ src/juraff/minecraft/manhunt/Main.java
@@ -4,4 +4,10 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin {
 
+	
+	//My first program
+	public static void main(String[] args)
+	{
+		System.out.println("Hello, World!");
+	}
 }
