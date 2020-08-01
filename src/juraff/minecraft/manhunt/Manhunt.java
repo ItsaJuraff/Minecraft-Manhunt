ac46abd5d3f4a7070cd967a7801934a13b389343
John Madden <jmadden173@protonmail.com>
2020-08-01 00:42:24 -0700

    Created basic spigot plugin

---
+++ src/juraff/minecraft/manhunt/Manhunt.java
@@ -0,0 +1,15 @@
+package juraff.minecraft.manhunt;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Manhunt extends JavaPlugin {
+	@Override
+	public void onEnable() {
+		
+	}
+	
+	@Override
+	public void onDisable() {
+		
+	}
+
+}
