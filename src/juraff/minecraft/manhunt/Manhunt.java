2e2aeb3df9322ad7df45404ce2f0a2d485f8c5f5
John Madden <jmadden173@protonmail.com>
2020-08-01 11:11:02 -0700

    Registered command with spigot

--- src/juraff/minecraft/manhunt/Manhunt.java
+++ src/juraff/minecraft/manhunt/Manhunt.java
@@ -4,8 +4,12 @@ import org.bukkit.plugin.java.JavaPlugin;
 public class Manhunt extends JavaPlugin {
 	@Override
 	public void onEnable() {
+		// create object to holds teams
+		Team team = new Team();
+		
 		// register commands
 		this.getCommand("test").setExecutor(new CommandTest());
+		this.getCommand("team").setExecutor(new CommandTeam(team));
 	}
 	
 	@Override
